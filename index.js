51fb640f6ced3491b7c65cfd285f570206ea1ac8
Muna-kim <63827788+Muna-kim@users.noreply.github.com>
2021-01-04 23:44:03 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://Muna:abcd1234@boilerplate.ioepm.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
+    }).then(()=>console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello, world 안녕, 세상아'))
+
+app.listen(port, () => console.log(`Example app listing on port ${port}!`))
