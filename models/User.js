e0068723e7bdd4d7bf6c91d728de1adb270f0783
Muna-kim <63827788+Muna-kim@users.noreply.github.com>
2021-01-06 21:05:31 +0900

    비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+//const bcrypt = require('bcrypt');
+//const saltRounds = 10
+
 
 const userSchema = mongoose.Schema({
     name:{
         type : String,
         maxlehgth: 50
     },
-    emali:{
+    email:{
         type : String,
         trim : true,
         
@@ -31,6 +34,31 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+// userSchema.pre('save',function(next){
+//     //비밀번호를 암호화 시킨다.
+//     // bcrypt.genSalt(saltRounds, function(err, salt) {
+//     //     bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+//     //         // Store hash in your password DB.
+//     //     });
+//     // });
+
+//     var user = this;
+
+//     if(user.isModified('password')){
+
+//         bcrypt.genSalt(saltRounds,function(err,salt){
+//             if(err) return next(err)
+    
+//             bcrypt.hash(user.password , salt, function(err,hash){
+//                 if(err) return next(err)
+//                 user.password = hash
+//                 next()
+//             })
+//         })
+//     }
+
+// })
 const User = mongoose.model('User',userSchema)
 
 module.exports={User}
